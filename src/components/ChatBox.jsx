936cf1a3979a754fc73225aa0acc11a67f1a1d77
Affan Rashid <muhammadaffan9800@gmail.com>
2024-01-12 10:07:48 -0800

    ui fixes

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,54 +1,59 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useRef, useState } from "react";
 import {
-    query,
-    collection,
-    orderBy,
-    onSnapshot,
-    limit,
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
 } from "firebase/firestore";
-import { fireStore } from '../firebase';
-import Navbar from './Navbar'
-import SendMessage from './SendMessage';
-import Message from './Message';
-import { Box, Paper } from '@mui/material';
+import { fireStore } from "../firebase";
+import Navbar from "./Navbar";
+import SendMessage from "./SendMessage";
+import Message from "./Message";
+import { Box, Paper } from "@mui/material";
 
 const ChatBox = () => {
-    const [messages, setMessages] = useState([]);
-    const scroll = useRef();
+  const [messages, setMessages] = useState([]);
+  const scroll = useRef();
 
-    useEffect(() => {
-        const q = query(
-            collection(fireStore, "messages"),
-            orderBy("createdAt", "desc"),
-            limit(50)
-        );
-        const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
-            const fetchedMessages = [];
-            QuerySnapshot.forEach((doc) => {
-                fetchedMessages.push({ ...doc.data(), id: doc.id });
-            });
-            const sortedMessages = fetchedMessages.sort(
-                (a, b) => a.createdAt - b.createdAt
-            );
-            setMessages(sortedMessages);
-        });
-        return () => unsubscribe;
-    }, []);
-    return (
-        <>
-            <Box sx={{ minHeight: "100vh", bgcolor: "background.default", padding: "0 10px" }} >
-                <Navbar />
-                <Box>
-                    {messages?.map((message) => (
-                        <Message key={message.id} message={message} />
-                    )).reverse()}
-                </Box>
-                {/* <span ref={scroll}></span> */}
-                <SendMessage scroll={scroll} />
-            </Box>
-        </>
-    )
-}
+  useEffect(() => {
+    const q = query(
+      collection(fireStore, "messages"),
+      orderBy("createdAt", "desc"),
+      limit(50)
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      const fetchedMessages = [];
+      QuerySnapshot.forEach((doc) => {
+        fetchedMessages.push({ ...doc.data(), id: doc.id });
+      });
+      const sortedMessages = fetchedMessages.sort(
+        (a, b) => a.createdAt - b.createdAt
+      );
+      setMessages(sortedMessages);
+    });
+    return () => unsubscribe;
+  }, []);
+  return (
+    <>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          bgcolor: "background.default",
+        }}
+      >
+        <Navbar />
+        <Box padding="0 10px">
+          {messages?.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
+        </Box>
+        <span ref={scroll}></span>
+        <SendMessage scroll={scroll} />
+      </Box>
+    </>
+  );
+};
 
-export default ChatBox
+export default ChatBox;
--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,70 +1,73 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { auth, fireStore } from "../firebase";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import {
-    Box,
-    Button,
-    IconButton,
-    Input,
-    InputBase,
-    Stack,
+  Box,
+  Button,
+  IconButton,
+  Input,
+  InputBase,
+  Stack,
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
-const SendMessage = () => {
-    const [message, setMessage] = useState("");
+const SendMessage = ({ scroll }) => {
+  const [message, setMessage] = useState("");
 
-    const sendMessage = async (event) => {
-        event.preventDefault();
-        if (message.trim() === "") {
-            alert("Enter valid message");
-            return;
-        }
-        const { uid, displayName, photoURL } = auth.currentUser;
-        await addDoc(collection(fireStore, "messages"), {
-            text: message,
-            name: displayName,
-            avatar: photoURL,
-            createdAt: serverTimestamp(),
-            uid,
-        });
-        setMessage("");
-    };
+  const sendMessage = async (event) => {
+    event.preventDefault();
+    if (message.trim() === "") {
+      alert("Enter valid message");
+      return;
+    }
+    const { uid, displayName, photoURL } = auth.currentUser;
+    await addDoc(collection(fireStore, "messages"), {
+      text: message,
+      name: displayName,
+      avatar: photoURL,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+    scroll.current.scrollIntoView({ behavior: "smooth" });
+    setMessage("");
+  };
 
-    return (
-        <Box>
-            <form onSubmit={(e) => sendMessage(e)} style={{ width: "100%" }}>
-                <Stack
-                    direction="row"
-                    width='98%'
-                    sx={{
-                        position: "fixed",
-                        bottom: 10,
-                        border: "2px solid #d4d4d4",
-                        p: 1,
-                        // margin: "0 10px",
-                        borderRadius: 5,
-                        // zIndex: 2,
-                        bgcolor: "background.default"
-                    }}
-                    alignItems="center"
-                    justifyContent="space-between"
-                >
-                    <InputBase
-                        name="messageInput"
-                        type="text"
-                        placeholder="Type message..."
-                        onChange={(e) => setMessage(e.target.value)}
-                        value={message}
-                        fullWidth
-                    />
-                    <IconButton type="submit">
-                        <SendIcon />
-                    </IconButton>
-                </Stack>
-            </form>
-        </Box>
-
-    );
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        p: 2,
+        bgcolor: "background.default",
+      }}
+      width="100%"
+    >
+      <form onSubmit={(e) => sendMessage(e)} style={{ width: "100%" }}>
+        <Stack
+          direction="row"
+          sx={{
+            border: "2px solid #d4d4d4",
+            p: 1,
+            borderRadius: 5,
+            bgcolor: "background.default",
+          }}
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <InputBase
+            name="messageInput"
+            type="text"
+            placeholder="Type message..."
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            fullWidth
+          />
+          <IconButton type="submit">
+            <SendIcon />
+          </IconButton>
+        </Stack>
+      </form>
+    </Box>
+  );
 };
 export default SendMessage;
