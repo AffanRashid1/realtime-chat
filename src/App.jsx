6476a29cfc4fbe86754832f2686018c6254dbdf8
Affan Rashid <muhammadaffan9800@gmail.com>
2024-01-13 06:18:39 -0800

    fixes in auth  response

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './firebase';
-import Login from './components/Login';
-import ChatBox from './components/ChatBox';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Login from "./components/Login";
+import ChatBox from "./components/ChatBox";
 import Splash from "./screen/Splash";
 import { createTheme, ThemeProvider } from "@mui/material";
 
-
 function App() {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
 
   const theme = createTheme({
     palette: {
-      type: 'dark',
+      type: "dark",
       primary: {
-        main: '#071A2B',
-        light: '#e8dfde',
+        main: "#071A2B",
+        light: "#e8dfde",
       },
       secondary: {
-        main: '#162C46',
+        main: "#162C46",
         // main: '#0842A0',
       },
       background: {
-        default: '#031525',
-        paper: '#162C46',
+        default: "#031525",
+        paper: "#162C46",
       },
       text: {
-        primary: '#fff',
+        primary: "#fff",
       },
       action: {
         active: "#C7C4C6",
@@ -36,19 +35,14 @@ function App() {
     },
   });
 
-
-
   return (
     <ThemeProvider theme={theme}>
-      {/* {
-        !user && <Splash />
-      } */}
+      {loading && <Splash />}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={!user ? <Login /> : <ChatBox />} />;
         </Routes>
       </BrowserRouter>
-
     </ThemeProvider>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,78 +1,29 @@
-import React from 'react'
-import { useAuthState } from "react-firebase-hooks/auth"
-import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
-import { auth } from '../firebase';
-import { Button, Stack, Typography } from '@mui/material';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { auth } from "../firebase";
+import { Button, Stack, Typography } from "@mui/material";
 
 const Login = () => {
-    const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
-    const googleSignIn = () => {
-        const provider = new GoogleAuthProvider();
-        signInWithRedirect(auth, provider);
-    };
-    // const signOut = () => {
-    //     auth.signOut();
-    // };
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
 
-    return (
-        <Stack>
-            {user ? (
-                <Button onClick={signOut} className="sign-out" type="button">
-                    Sign Out
-                </Button>
-            ) : (
-                <Button onClick={googleSignIn} variant='contained'>
-                    Continue With google
-                </Button>
-            )}
-
-        </Stack>
-    );
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <Button onClick={googleSignIn} variant="contained">
+        Continue With google
+      </Button>
+    </Stack>
+  );
 };
 
-export default Login
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from "react";
-// import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
-
-// const Login = () => {
-//   const [user, setUser] = useState(false);
-//   const googleSignIn = () => {
-//     setUser(true);
-//   };
-//   const signOut = () => {
-//     setUser(false);
-//   };
-//   return (
-//     <nav className="nav-bar">
-//       <h1>React Chat</h1>
-//       {user ? (
-//         <button onClick={signOut} className="sign-out" type="button">
-//           Sign Out
-//         </button>
-//       ) : (
-//         <button className="sign-in">
-//           <img
-//             onClick={googleSignIn}
-//             src={GoogleSignin}
-//             alt="sign in with google"
-//             type="button"
-//           />
-//         </button>
-//       )}
-//     </nav>
-//   );
-// };
-// export default Login;
+export default Login;
