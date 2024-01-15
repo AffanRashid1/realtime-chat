afe104d107817612ebaf09643bb09d741efab347
Affan <muhammadaffan9800@gmail.com>
2024-01-15 15:59:28 +0000

    make sidebar

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,37 +3,39 @@ import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Avatar, Box, Stack, Typography } from "@mui/material";
 const Message = ({ message }) => {
-  const [user] = useAuthState(auth);
+    const [user] = useAuthState(auth);
 
-  return (
-    <Stack
-      direction="row"
-      gap={2}
-      m="20px"
-      p={1}
-      sx={{
-        bgcolor: "secondary.main",
-        width: "max-content",
-        maxWidth: { xs: "100%", sm: "50%" },
-        borderRadius: 4,
-        marginLeft: message?.uid === user.uid && "auto",
-      }}
-    >
-      <Avatar
-        className="chat-bubble__left"
-        src={message?.avatar}
-        alt="user avatar"
-        sx={{ width: 24, height: 24 }}
-      />
-      <Box>
-        <Typography variant="caption" color="white">
-          {message?.name === user?.displayName ? "You" : message?.name}
-        </Typography>
-        <Typography variant="body1" color="white" fontSize={{ xs: 13, sm: 15 }}>
-          {message.text}
-        </Typography>
-      </Box>
-    </Stack>
-  );
+    return (
+        <Stack
+            direction="row"
+            gap={2}
+            // className={`${message.uid === user.uid ? "right" : ""}`}
+            m="20px"
+            p={1}
+            sx={{
+                bgcolor: "secondary.main",
+                width: "max-content",
+                maxWidth: "50%",
+                borderRadius: 4,
+                marginLeft: message?.uid === user.uid && "auto",
+
+
+            }}
+        >
+            <Avatar
+                className="chat-bubble__left"
+                src={message?.avatar}
+                alt="user avatar"
+            />
+            <Box>
+                <Typography variant="caption" color="white">
+                    {message?.name === user?.displayName ? "You" : message?.name}
+                </Typography>
+                <Typography variant="body1" color="white">
+                    {message.text}
+                </Typography>
+            </Box>
+        </Stack >
+    );
 };
 export default Message;
