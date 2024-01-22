48fe4fde025d0c9185e7aa335f213a967d153b46
Affan Rashid <muhammadaffan9800@gmail.com>
2024-01-22 08:56:52 -0800

    completed

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,63 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import { Avatar, Box, LinearProgress, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, Paper } from '@mui/material'
-import { collection, onSnapshot } from 'firebase/firestore';
-import { auth, fireStore } from '../../firebase';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import {
+  Avatar,
+  Box,
+  LinearProgress,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Paper,
+} from "@mui/material";
+import { collection, onSnapshot } from "firebase/firestore";
+import { auth, fireStore } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 const SideBar = () => {
+  const [users, setUsers] = useState(null);
+  const navigate = useNavigate();
+  const user = auth?.currentUser;
 
-    const [users, setUsers] = useState(null)
-    const navigate = useNavigate()
-    const user = auth?.currentUser
+  useEffect(() => {
+    const unSub = onSnapshot(collection(fireStore, "users"), (snapshot) => {
+      setUsers(snapshot.docs.map((doc) => doc.data()));
+    });
+    return unSub;
+  }, []);
 
-    useEffect(() => {
-        const unSub = onSnapshot(collection(fireStore, "users"), (snapshot) => {
-            setUsers(snapshot.docs.map((doc) => doc.data()));
-        });
-        return unSub;
-    }, []);
+  const handleToggle = (value) => {
+    const serializedValue = encodeURIComponent(JSON.stringify(value));
+    navigate(`/chat-home/${serializedValue}`);
+  };
 
-    const handleToggle = (value) => {
-        const serializedValue = encodeURIComponent(JSON.stringify(value));
-        navigate(`/chat-home/${serializedValue}`);
-    };
-
-
-    return (
-        <>
-            <Paper sx={{ width: "100%", maxHeight: "100vh", height: "100vh", }}>
-                <List
-                    sx={{
-                        width: "100%",
-                    }}
+  return (
+    <>
+      <Paper
+        sx={{
+          width: "100%",
+          maxHeight: "100vh",
+          height: "100vh",
+          wordBreak: "break-word",
+        }}
+      >
+        <List
+          sx={{
+            width: "100%",
+          }}
+        >
+          {users?.map((value) => {
+            const labelId = `checkbox-list-secondary-label-${value}`;
+            if (user?.uid !== value.userId)
+              return (
+                <ListItem
+                  key={value.userId}
+                  disablePadding
+                  sx={{ padding: "7px 0" }}
                 >
-                    {users?.map((value) => {
-                        const labelId = `checkbox-list-secondary-label-${value}`;
-                        if (user?.uid !== value.userId)
+                  <ListItemButton
+                    onClick={() => {
+                      handleToggle(value);
+                    }}
+                  >
+                    <ListItemAvatar>
+                      <Avatar alt={value.username} src={value?.avatar} />
+                    </ListItemAvatar>
+                    <ListItemText id={labelId} primary={`${value.username}`} />
+                  </ListItemButton>
+                </ListItem>
+              );
+          })}
+        </List>
+      </Paper>
+    </>
+  );
+};
 
-                            return (
-                                <ListItem key={value.userId} disablePadding sx={{ padding: "7px 0" }}>
-                                    <ListItemButton
-                                        onClick={() => {
-                                            handleToggle(value);
-                                        }}
-                                    >
-                                        <ListItemAvatar>
-                                            <Avatar
-                                                alt={value.username}
-                                                src={value?.avatar}
-                                            />
-                                        </ListItemAvatar>
-                                        <ListItemText id={labelId}
-                                            primary={`${value.username}`} />
-                                    </ListItemButton>
-                                </ListItem>
-                            );
-                    })}
-                </List>
-            </Paper>
-        </>
-    )
-}
-
-export default SideBar
+export default SideBar;
